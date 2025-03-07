3c603fa68f659d16f5f8dd439e8838826d4f65aa
Dennis Zhang <dennis.zhang@parks.nyc.gov>
2025-03-07 16:46:36 -0500

    update gcp outputs

--- gcp/output.tf
+++ gcp/output.tf
@@ -1,3 +1,24 @@
-output "service_url" {
-  value = google_cloud_run_service.service.status[0].url
+output "kubernetes_cluster_name" {
+  value       = google_container_cluster.primary.name
+  description = "GKE Cluster Name"
+}
+
+output "kubernetes_cluster_host" {
+  value       = google_container_cluster.primary.endpoint
+  description = "GKE Cluster Host"
+}
+
+output "kubernetes_namespace" {
+  value       = kubernetes_namespace.app.metadata[0].name
+  description = "Kubernetes Namespace"
+}
+
+output "kubernetes_service_url" {
+  value       = kubernetes_service.app.status.cluster_ip
+  description = "The URL of the Kubernetes service"
+}
+
+output "persistent_volume_claim" {
+  value       = kubernetes_persistent_volume_claim.app_pvc.metadata[0].name
+  description = "PVC Name"
 }
