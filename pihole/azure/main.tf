e104425609db67ef9dd9e61084a818ae31885414
Dennis Zhang <dennis.zhang@parks.nyc.gov>
2025-03-28 16:13:38 -0400

    fix aci name

--- pihole/azure/main.tf
+++ pihole/azure/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_storage_share" "share" {
 }
 
 resource "azurerm_container_group" "aci" {
-  name                = var.app_name + "-container"
+  name                = var.app_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   ip_address_type     = "Public"
