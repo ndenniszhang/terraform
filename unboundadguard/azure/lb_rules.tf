abf13bc5063a9b1841e0e1fa0361ae9d51a7c3ed
Dennis Zhang <dennis.zhang@parks.nyc.gov>
2025-05-01 17:05:15 -0400

    fix lb rules

---
+++ unboundadguard/azure/lb_rules.tf
@@ -0,0 +1,99 @@
+resource "azurerm_lb_rule" "lb_rule_dns_udp" {
+  name                           = "${var.common_name}-rule-dns"
+  loadbalancer_id                = azurerm_lb.default.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+  protocol                       = "Udp"
+  frontend_port                  = 53
+  backend_port                   = 53
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.default.id]
+  probe_id                       = azurerm_lb_probe.lb_probe_dns.id
+}
+
+resource "azurerm_lb_probe" "lb_probe_dns" {
+  name                = "${var.common_name}-probe-dns"
+  loadbalancer_id     = azurerm_lb.default.id
+  protocol            = "Tcp"
+  port                = 53
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+resource "azurerm_lb_rule" "lb_rule_http" {
+  name                           = "${var.common_name}-rule-http"
+  loadbalancer_id                = azurerm_lb.default.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.default.id]
+  probe_id                       = azurerm_lb_probe.lb_probe_http.id
+}
+
+resource "azurerm_lb_probe" "lb_probe_http" {
+  name                = "${var.common_name}-probe-http"
+  loadbalancer_id     = azurerm_lb.default.id
+  protocol            = "Tcp"
+  port                = 80
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+resource "azurerm_lb_rule" "lb_rule_dnstls" {
+  name                           = "${var.common_name}-rule-dot"
+  loadbalancer_id                = azurerm_lb.default.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+  protocol                       = "Tcp"
+  frontend_port                  = 853
+  backend_port                   = 853
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.default.id]
+  probe_id                       = azurerm_lb_probe.lb_probe_dnstls.id
+}
+
+resource "azurerm_lb_probe" "lb_probe_dnstls" {
+  name                = "${var.common_name}-probe-dot"
+  loadbalancer_id     = azurerm_lb.default.id
+  protocol            = "Tcp"
+  port                = 853
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+resource "azurerm_lb_rule" "lb_rule_https" {
+  name                           = "${var.common_name}-rule-https"
+  loadbalancer_id                = azurerm_lb.default.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+  protocol                       = "Tcp"
+  frontend_port                  = 443
+  backend_port                   = 443
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.default.id]
+  probe_id                       = azurerm_lb_probe.lb_probe_https.id
+}
+
+resource "azurerm_lb_probe" "lb_probe_https" {
+  name                = "${var.common_name}-probe-https"
+  loadbalancer_id     = azurerm_lb.default.id
+  protocol            = "Tcp"
+  port                = 443
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+resource "azurerm_lb_rule" "lb_rule_setup" {
+  name                           = "${var.common_name}-rule-setup"
+  loadbalancer_id                = azurerm_lb.default.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+  protocol                       = "Tcp"
+  frontend_port                  = 3000
+  backend_port                   = 3000
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.default.id]
+  probe_id                       = azurerm_lb_probe.lb_probe_setup.id
+}
+
+resource "azurerm_lb_probe" "lb_probe_setup" {
+  name                = "${var.common_name}-probe-setup"
+  loadbalancer_id     = azurerm_lb.default.id
+  protocol            = "Tcp"
+  port                = 3000
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
--- unboundadguard/azure/network.tf
+++ unboundadguard/azure/network.tf
@@ -17,7 +17,7 @@ resource "azurerm_subnet" "aci_subnet" {
     service_delegation {
       name = "Microsoft.ContainerInstance/containerGroups"
       actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action"
+        "Microsoft.Network/virtualNetworks/subnets/action"
       ]
     }
   }
@@ -46,72 +46,4 @@ resource "azurerm_lb" "default" {
 resource "azurerm_lb_backend_address_pool" "default" {
   name            = "${var.common_name}-address-pool"
   loadbalancer_id = azurerm_lb.default.id
-}
-
-resource "azurerm_lb_rule" "lb_rule_dns_udp" {
-  name                           = "${var.common_name}-rule-dns-udp"
-  loadbalancer_id                = azurerm_lb.default.id
-  frontend_ip_configuration_name = "PublicIPAddress"
-  protocol                       = "Udp"
-  frontend_port                  = 53
-  backend_port                   = 53
-}
-
-# resource "azurerm_lb_rule" "lb_rule_http" {
-#   name                           = "${var.common_name}-rule-http"
-#   loadbalancer_id                = azurerm_lb.default.id
-#   frontend_ip_configuration_name = "PublicIPAddress"
-#   protocol                       = "Tcp"
-#   frontend_port                  = 80
-#   backend_port                   = 80
-#   # probe_id                       = azurerm_lb_probe.lb_probe_http.id
-# }
-
-# resource "azurerm_lb_probe" "lb_probe_http" {
-#   name                = "${var.common_name}-probe-http"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   loadbalancer_id     = azurerm_lb.lb.id
-#   protocol            = "Tcp"
-#   port                = 80
-#   interval_in_seconds = 5
-#   number_of_probes    = 2
-# }
-
-resource "azurerm_lb_rule" "lb_rule_dnstls" {
-  name                           = "${var.common_name}-rule-dnstls"
-  loadbalancer_id                = azurerm_lb.default.id
-  frontend_ip_configuration_name = "PublicIPAddress"
-  protocol                       = "Tcp"
-  frontend_port                  = 853
-  backend_port                   = 853
-  # probe_id                       = azurerm_lb_probe.lb_probe_https.id
-}
-
-resource "azurerm_lb_rule" "lb_rule_https" {
-  name                           = "${var.common_name}-rule-https"
-  loadbalancer_id                = azurerm_lb.default.id
-  frontend_ip_configuration_name = "PublicIPAddress"
-  protocol                       = "Tcp"
-  frontend_port                  = 443
-  backend_port                   = 443
-  # probe_id                       = azurerm_lb_probe.lb_probe_https.id
-}
-
-# resource "azurerm_lb_probe" "lb_probe_https" {
-#   name                = "${var.common_name}-probe-https"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   loadbalancer_id     = azurerm_lb.lb.id
-#   protocol            = "Tcp"
-#   port                = 443
-#   interval_in_seconds = 5
-#   number_of_probes    = 2
-# }
-
-resource "azurerm_lb_rule" "lb_rule_setup" {
-  name                           = "${var.common_name}-rule-setup"
-  loadbalancer_id                = azurerm_lb.default.id
-  frontend_ip_configuration_name = "PublicIPAddress"
-  protocol                       = "Tcp"
-  frontend_port                  = 3000
-  backend_port                   = 3000
 }
